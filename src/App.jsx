9a42d4eeee655b2d7fb13ea368bccb3e5b133330
unknown <you@example.com>
2024-11-13 14:16:35 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { React,  useEffect } from 'react';
+import { React, lazy,  useEffect } from 'react';
 import { fetchContacts } from './redux/contactsOps';
 import { useDispatch } from 'react-redux';
 import './App.css';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from './components/ContactForm/ContactForm';
+import { lazy } from 'react';
+
+
+const HomePage = lazy(
+  () => import('./pages/HomePage/HomePage')
+);
+const ContactsPage = lazy(() => import('./pages/ContactsPage/ContactsPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const RegistrationPage = lazy(() =>
+  import('./pages/RegistrationPage/RegistrationPage')
+);
+
 
 function App() {
   const dispatch = useDispatch();
@@ -14,12 +26,19 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="//login" element={<LoginPage />} />
+      <Route path="//contacts" element={<ContactsPage />} />
+    </Routes>
+
+    //   <div>
+    //     <h1>Phonebook</h1>
+    //     <ContactForm />
+    //     <SearchBox />
+    //     <ContactList />
+    //   </div>
   );
 }
 
